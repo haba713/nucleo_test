282abf4d4a3beba29b4a297317435687ace49c48
Harri T. <haba713@gmail.com>
2025-07-01 08:42:40 +0300

    feat: control user leds

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,47 @@ use stm32f4xx_hal::{
 };
 use cortex_m::delay::Delay;
 
+#[derive(Clone, Copy)]
+enum LedId {
+    Led1,
+    Led2,
+    Led3,
+}
+
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(16.MHz()).freeze();
-    let gpioa = dp.GPIOA.split();
-    let mut led = gpioa.pa5.into_push_pull_output();
+    let gpiob = dp.GPIOB.split();
+
+    let mut led1 = gpiob.pb0.into_push_pull_output();
+    let mut led2 = gpiob.pb7.into_push_pull_output();
+    let mut led3 = gpiob.pb14.into_push_pull_output();
+
+    led1.set_low();
+    led2.set_low();
+    led3.set_low();
+
+    let sequence = [LedId::Led1, LedId::Led2, LedId::Led3, LedId::Led2];
+
     let mut delay = Delay::new(cp.SYST, clocks.hclk().raw());
+    let mut index = 0;
+
     loop {
-        led.toggle();
-        delay.delay_ms(2000_u32);
+        led1.set_low();
+        led2.set_low();
+        led3.set_low();
+
+        match sequence[index] {
+            LedId::Led1 => led1.set_high(),
+            LedId::Led2 => led2.set_high(),
+            LedId::Led3 => led3.set_high(),
+        }
+
+        delay.delay_ms(200_u32);
+
+        index = (index + 1) % sequence.len();
     }
 }
